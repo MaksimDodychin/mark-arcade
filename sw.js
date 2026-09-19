21e86cf614eb68787aba86e9dfe96fcaa0ee427f
Максим Добычин <dobyvankenoby@gmail.com>
2026-09-19 20:49:50 +0300

    Планета игр: рейтинг крутости вдвоём (9 игр) + оценка вдвоём на карточке каждой игры

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Scoped offline application updater; version changes with the payload and updater.
-const VERSION = 'a2a4e79791';
+const VERSION = 'dc433ea3aa';
 const PREFIX = "markgames-";
 const CACHE = PREFIX + VERSION;
 const SCOPE = new URL(self.registration.scope);
